2489b670172054276a20f79766de3ec38dc06dba
bigeatman <dudgp2030@gmail.com>
2022-06-23 18:24:22 +0900

    ADD CD

---
+++ java/src/ch05/ex05/case12/Main.java
@@ -0,0 +1,12 @@
+package ch05.ex05.case12;
+
+public class Main {
+	public static void main(String[] args) {
+		Man.say("I hate leator");
+		// Man.tell("");
+
+		Man man = new Man();
+		man.tell("I hate he");
+		man.say("Hello");
+	}
+}
---
+++ java/src/ch05/ex05/case12/Man.java
@@ -0,0 +1,11 @@
+package ch05.ex05.case12;
+
+public class Man {
+	public static void say(String msg) {
+		System.out.println(msg);
+	}
+
+	public void tell(String msg) {
+		System.out.println(msg);
+	}
+}
