e4b65fa701a0af3c034f6259c1f0f11c40591a87
bigeatman <dudgp2030@gmail.com>
2022-06-24 11:20:52 +0900

    add cd

---
+++ java/src/ch05/home/ex06/case02/Console.java
@@ -0,0 +1,57 @@
+package ch05.home.ex06.case02;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Console {
+	private static Scanner sc;
+
+	static {
+		sc = new Scanner(System.in);
+	}
+
+	private static void inMsg(String msg) {
+		System.out.print(msg + ">");
+	}
+
+	public static String inStr(String msg) {
+		Console.inMsg(msg);
+		String input = sc.nextLine().trim();
+
+		if (input.length() == 1) {
+			System.out.println("ERROR] 두개 이상의 문자를 입력하세요.");
+			return inStr(msg);
+		} else {
+			return input;
+		}
+	}
+
+	public static int inNum(String msg) {
+		int num = 0;
+
+		try {
+			Console.inMsg(msg);
+			num = sc.nextInt();
+			sc.nextLine();
+
+			if (num >= 10) {
+				System.out.println("ERROR] 한자리 자연수를 입력하세요.");
+				return inNum(msg);
+			} else {
+				return num;
+			}
+		} catch (InputMismatchException e) {
+			System.out.println("ERROR] 숫자를 입력하세요.");
+			sc.nextLine();
+			return inNum(msg);
+		}
+	}
+
+	public static void info(String msg) {
+		System.out.println(msg);
+	}
+
+	public static void err(String msg) {
+		System.out.println("ERROR");
+	}
+}
---
+++ java/src/ch05/home/ex06/case02/Main.java
@@ -0,0 +1,16 @@
+package ch05.home.ex06.case02;
+
+public class Main {
+	public static void main(String[] args) {
+		String inputStr = Console.inStr("문자열 입력: ");
+		int inputNum = Console.inNum("숫자 입력: ");
+		Console.err("Power off");
+	}
+}
+
+/**
+ * 콘솔 예외처리 (type missmatch) <br>
+ * 문자열: 그대로 문자열 <br>
+ * 숫자: 한자리d
+ * 
+ */
